d69f9c2ca14236d222d6d9186dab5d292b4f19ee
Furkan <furknataman@gmail.com>
2022-07-04 17:39:53 +0300

    appbar

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -1,6 +1,5 @@
 import 'package:english/pages/main.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
