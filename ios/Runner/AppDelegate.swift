5baea4fe515d100623af23ceae8701fa402e4f22
furkan <furknataman@gmail.com>
2026-03-19 23:06:19 +0300

    chore: update iOS project configuration and dependencies
    - Update iOS platform version from 12.0 to 16.0 in Podfile.
    - Upgrade Firebase dependencies to version 12.9.0 and update related plugins.
    - Remove deprecated MinimumOSVersion key from AppFrameworkInfo.plist.
    - Change AppDelegate annotation from @UIApplicationMain to @main.
    - Adjust project settings in project.pbxproj, including deployment target and object version.
    - Add new build phase for copying Pods resources.
    - Update generated plugin registrations for new dependencies in macOS project.
    
    This commit enhances compatibility with the latest iOS features and improves dependency management.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import Firebase
 import FirebaseCore
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
